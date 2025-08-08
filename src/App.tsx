54aedc257214c6537fe3f46574c3cdaba000e164
Ernesto Gutierrez <ergutierrez39@gmail.com>
2025-08-08 07:44:23 -0600

    feat: capstone init

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function App() {
+  return (
+    <React.Fragment>
+      <p>Welcome to React with TypeScript project!</p>
+    </React.Fragment>
+  )
+}
+
+export default App
